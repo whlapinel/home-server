2daa658adc0f34835426d7b87cda880b47e31538
Will Lapinel <whlapinel@gmail.com>
2026-04-06 16:42:09 -0700

    add script to pull param into ec2 instance

---
+++ pull-remote-param.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+aws ssm get-parameter \
+  --name "/home-server/.env" \
+  --with-decryption \
+  --query 'Parameter.Value' \
+  --output text \
+  --region us-east-2 \
+  > /etc/home-server/.env
